6d5169e48f4ef7d958486a6d089e70e7000309bd
zsummer <yawei.zhang@foxmail.com>
2023-07-06 20:04:09 +0800

    foreach

---
+++ src/resume_frame/base_frame.cpp
@@ -0,0 +1,65 @@
+
+#include "base_frame.h"
+
+zshm_space* g_shm_space = nullptr;
+
+
+s32 BaseFrame::Config(FrameConf& conf)
+{
+    memset(&conf, 0, sizeof(conf));
+    conf.space_conf_.shm_key_ = 198709;
+    conf.space_conf_.use_heap_ = false;
+#ifdef WIN32
+
+#else
+    conf.space_conf_.use_fixed_ = 1;
+    conf.space_conf_.fixed_ = 0x00006AAAAAAAAAAAULL;
+    conf.space_conf_.fixed_ = 0x0000700000000000ULL;
+#endif // WIN32
+
+    PoolHelper helper;
+    helper.Attach(conf.pool_conf_, true);
+    s32 ret = helper.Add(23, 10, 100, "test");
+    if (ret != 0)
+    {
+        return ret;
+    }
+
+
+    conf.space_conf_.subs_[ShmSpace::kMainFrame].size_ = SPACE_ALIGN(sizeof(BaseFrame));
+    conf.space_conf_.subs_[ShmSpace::kPool].size_ = kPoolSpaceHeadSize + helper.TotalSpaceSize();
+    conf.space_conf_.subs_[ShmSpace::kBuddy].size_ = SPACE_ALIGN(zbuddy::zbuddy_size(kHeapSpaceOrder));
+    conf.space_conf_.subs_[ShmSpace::kMalloc].size_ = SPACE_ALIGN(zmalloc::zmalloc_size());
+    conf.space_conf_.subs_[ShmSpace::kHeap].size_ = SPACE_ALIGN(zbuddy_shift_size(kHeapSpaceOrder + kPageOrder));
+
+    conf.space_conf_.whole_.size_ += SPACE_ALIGN(sizeof(conf.space_conf_));
+    for (u32 i = 0; i < ZSHM_MAX_SPACES; i++)
+    {
+        conf.space_conf_.subs_[i].offset_ = conf.space_conf_.whole_.size_;
+        conf.space_conf_.whole_.size_ += conf.space_conf_.subs_[i].size_;
+    }
+
+
+    return 0;
+}
+
+
+s32 BaseFrame::Init()
+{
+    return 0;
+}
+
+s32 BaseFrame::Start()
+{
+    return 0;
+}
+
+s32 BaseFrame::Resume()
+{
+    return 0;
+}
+
+
+
+
+
---
+++ src/resume_frame/pool_foreach.h
@@ -0,0 +1,74 @@
+
+/*
+* Copyright (C) 2019 YaweiZhang <yawei.zhang@foxmail.com>.
+* All rights reserved
+* This file is part of the zframe, used MIT License.
+*/
+
+
+#ifndef POOL_FOREACH_H_
+#define POOL_FOREACH_H_
+
+#include "frame_def.h"
+#include "frame_option.h"
+
+
+
+
+using PoolHook = s32(*)(void*, s64);
+
+
+class ForeachInst
+{
+public:
+    inline s32 hook(const zforeach_impl::subframe& sub, u32 begin_id, u32 end_id, s64 now_ms)
+    {
+        PoolSpace* space = SubSpace<PoolSpace, kPool>();
+        for (s32 i = begin_id; i < end_id; i++)
+        {
+            hook_(space->pools_[pool_id_].user_data<char>(i), now_ms);
+        }
+        return 0;
+    }
+    s32 pool_id_;
+    PoolHook hook_;
+};
+using PoolForeach = zforeach<ForeachInst>;
+
+
+
+
+template<u32 MaxForeachs = 100>
+class PoolForeachs
+{
+public:
+
+    inline s32 add(u32 pool_id, u32 begin_id, u32 end_id, u32 base_frame_len, u32 long_frame_len, PoolHook hook)
+    {
+        if (foreachs_.full())
+        {
+            return -1;
+        }
+        PoolForeach f;
+        f.foreach_inst_.pool_id_ = pool_id;
+        f.foreach_inst_.hook_ = hook;
+        foreachs_.push_back(f);
+        s32 ret = foreachs_.back().init(0, begin_id, end_id, base_frame_len, long_frame_len);
+        return ret;
+    }
+
+
+    inline s32 window_foreach(s64 now_ms)
+    {
+        for (PoolForeach& f : foreachs_)
+        {
+            f.window_foreach(f.subframe_.soft_begin_, f.subframe_.soft_end_, now_ms);
+        }
+        return 0;
+    }
+
+private:
+    zarray<PoolForeach, MaxForeachs> foreachs_;
+};
+
+#endif
